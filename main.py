cf85553a968857d2687612b6ad51e1e388f9fca2
Corpsy667 <79983884+Corpsy667@users.noreply.github.com>
2023-05-07 13:32:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from data import *
 import numpy as np
 import data_analyse as da
 import regression_lineaire as rl
-from sklearn.linear_model import LinearRegression
-
+from sklearn.linear_model import LinearRegression, Ridge, Lasso
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
 
 # Press the green button in the gutter to run the script.
 X, Y, combined_data = da.dt.create_data()
@@ -19,8 +21,8 @@ print("abce")
 print("data : azezaezae ", usable_data)
 # données standardisées :
 standardized_data, Y_data = create_standardized_data(usable_data)
-print("données finales = ", standardized_data, Y_data)
-print(da.showACP(standardized_data, Y_data))
+#print("données finales = ", standardized_data, Y_data)
+#print(da.showACP(standardized_data, Y_data))
 da.showACP(standardized_data, Y_data)
 standardized_data_EDA = pd.concat([standardized_data, Y], axis = 1)
 
@@ -33,18 +35,41 @@ combined_data_used_DE = da.dt.separating_data(combined_data, 'DE')
 #Sert à voir quelles données seront à virer
 print(X.dtypes)
 
+print(standardized_data.shape, Y_data.shape)
 
 x_train1, x_test1, y_train1, y_test1 = rl.create_regression_data(standardized_data, Y_data)
-x_train, x_test, y_train, y_test = rl.prepare_data(usable_data)
 
-print("Xtrain = ", x_train, "Xtest",x_test, "ytrain", y_train, "ytest", y_test)
+print("ShapeXtrain1", x_train1.shape, "ShapeXtest", x_test1.shape, "ShapeYtrain1", y_train1.shape, "shapeytest", y_test1.shape)
+#print("Xtrain = ", x_train1, "Xtest",x_test1, "ytrain", y_train1, "ytest", y_test1)
 
-#Simple regression
-regression_result = rl.simple_regression(x_train, x_test, y_train, y_test)
+#evaluate model
+models = [
+    ('Linear Regression', LinearRegression()),
+    ('Ridge Regression', Ridge()),
+    ('Lasso Regression', Lasso()),
+    ('K-Nearest Neighbors', KNeighborsRegressor()),
+    ('Decision Tree', DecisionTreeRegressor()),
+    ('Random Forest', RandomForestRegressor())
+]
 
+accuracy = []
+for name, model in models:
+    print(f"{name}:")
+    mse, r2, spearman_corr = rl.train_and_evaluate_by_model(model, x_train1, x_test1, y_train1, y_test1)
+    accuracy.append((name, mse, r2, spearman_corr))
+    print(f"  R2: {r2:.4f}")
+    print(f"  MSE: {mse:.4f}")
+    print(f"  Spearman Correlation: {spearman_corr:.4f}\n")
+    if name != "K-Nearest Neighbors" and name != "Decision Tree" and name != "Random Forest":
+        rl.display_feat_imp_reg(model)
+
+# Comparez les performances des modèles
+results_df = pd.DataFrame(accuracy, columns=['Model', 'MSE', 'R2', 'Spearman Correlation'])
+results_df = results_df.sort_values(by='R2', ascending=False)
+print("Performance ranking:")
+print(results_df)
 
 #print(y_test_prediction)
 
 
-rl.display_feat_imp_reg(regression_result)
 # UAL_LOAD, FR_CONSUMP, DE_NUCLEAR
